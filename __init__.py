f6294155e52ded6adf32085a48440a52f6b25540
idan.magled <idan.magled@naturalint.com>
2019-05-17 00:03:38 +0300

    - Added ynet stuff.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
+import requests
 import time
 
 if __name__ == '__main__':
+    r = requests.get('https://ynet.co.il')
     for i in range(1000):
-        print(i)
-        time.sleep(3)
+        time.sleep(2)
+        print(r.status_code)
+        print(r.text)
